58635ab949cf4798452d8165fb7b75f7a4e1ac78
MUHAMMAD ARIF NAWAZ <71346804+Arif432@users.noreply.github.com>
2024-05-06 22:27:53 +0500

    Update UserModal.js

--- server/models/UserModal.js
+++ server/models/UserModal.js
@@ -1,32 +1,56 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     name: {
-        type : String, 
-        required : true,
-        // minlength: [3, 'Name should be at least 3 characters'],
-        // maxlength: [10, 'Name should be at most 10 characters'],
-        // unique: [true, 'username already exists']
+        type: String, 
+        required: true,
     },
     email: {
-        type:String,
-        required:true,
-        unique:[true,'Email already exists'],
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength: [6, 'Password should be at least 6 characters']
-    },
-    role : {
         type: String,
-        default: 'patient'
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['patient', 'doctor'], // Role must be either 'patient' or 'doctor'
+        // default: 'patient',
+    },
+    // Additional fields for patients
+    dateOfBirth: {
+        type: Date,
+        required: function() {
+            return this.role === 'patient'; // Date of Birth required only for patients
+        }
+    },
+    bloodType: {
+        type: String,
+        required: function() {
+            return this.role === 'patient'; // Blood type required only for patients
+        }
+    },
+    // Additional fields for doctors
+    cnic: {
+        type: String,
+        required: function() {
+            return this.role === 'doctor'; // CNIC required only for doctors
+        }
+    },
+    address: {
+        type: String,
+        required: function() {
+            return this.role === 'doctor'; // Address required only for doctors
+        }
     },
     avatar: {
         type: String,
-        default: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'
+        default: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
     },
 });
 
-const UserModal = mongoose.model('users', UserSchema);
-module.exports = UserModal;
+const UserModel = mongoose.model('users', UserSchema);
+module.exports = UserModel;
